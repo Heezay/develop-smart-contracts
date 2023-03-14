6abdc755182831782e3d827048fd8fac4424f744
Heezay <den_9912@mail.ru>
2023-03-14 23:47:09 +0600

    Add incrementer core logic

--- incrementer/lib.rs
+++ incrementer/lib.rs
@@ -3,67 +3,52 @@
 #[ink::contract]
 mod incrementer {
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
     pub struct Incrementer {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        value: i32,
     }
 
     impl Incrementer {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
+        pub fn new(init_value: i32) -> Self {
             Self { value: init_value }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default())
+            Self { value: 0 }
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn inc(&mut self, by: i32) {
+            self.value += by;
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
+        pub fn get(&self) -> i32 {
             self.value
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
+
         use super::*;
 
-        /// We test if the default constructor does its job.
         #[ink::test]
         fn default_works() {
-            let incrementer = Incrementer::default();
-            assert_eq!(incrementer.get(), false);
+            let contract = Incrementer::default();
+            assert_eq!(contract.get(), 0);
         }
 
-        /// We test a simple use case of our contract.
         #[ink::test]
         fn it_works() {
-            let mut incrementer = Incrementer::new(false);
-            assert_eq!(incrementer.get(), false);
-            incrementer.flip();
-            assert_eq!(incrementer.get(), true);
+            let mut contract = Incrementer::new(42);
+            assert_eq!(contract.get(), 42);
+            contract.inc(5);
+            assert_eq!(contract.get(), 47);
+            contract.inc(-50);
+            assert_eq!(contract.get(), -3);
         }
     }
 }
